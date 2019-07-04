164f117cb434be7910348f1776134cdc8c9bbb9d
Rick Louie <rvlouie@Ricks-MacBook-Air.local>
2019-07-03 21:41:10 -0400

    Music page styled whoo

--- components/SouncloudPlayer/SoundcloudPlayer.js
+++ components/SouncloudPlayer/SoundcloudPlayer.js
@@ -1,5 +1,4 @@
 import { Component } from 'react';
-import PropTypes from 'prop-types';
 import { withSoundCloudAudio } from 'react-soundplayer/addons';
 import {
   PlayButton,
@@ -65,10 +64,11 @@ class PlaylistSoundPlayer extends Component {
       return (
         <button
           key={tracks.id}
-          className=""
+          className="player__track"
           onClick={this.playTrackAtIndex.bind(this, i)}>
-          <span className="">{tracks.user.username} - {tracks.title}</span>
-          <span className="">{Timer.prettyTime(tracks.duration / 1000)}</span>
+            <img className="player__track-image" src={tracks.artwork_url ? tracks.artwork_url : tracks.user.avatar_url} />      
+            <span className="player__track-title">{tracks.title}</span>
+            <span className="player__track-duration">{Timer.prettyTime(tracks.duration / 1000)}</span>
         </button>
       );
     });
@@ -86,33 +86,40 @@ class PlaylistSoundPlayer extends Component {
         <div className="">
           <div className="">
             <h2 className="">{tracks ? tracks.user.username : ''}</h2>
-            <Timer className="" duration={duration || 0} currentTime={currentTime} {...this.props} />
           </div>
-          <h2 className="">{tracks ? tracks.title : ''}</h2>
-          <div className="">
-            <PrevButton
-              className=""
-              onPrevClick={this.prevIndex.bind(this)}
-              {...this.props} />
-            <PlayButton
-              className="player__playbutton"
-              {...this.props} />
-            <NextButton
-              className=""
-              onNextClick={this.nextIndex.bind(this)}
-              {...this.props} />
-            <VolumeControl
-              className=""
-              buttonClassName=""
-              {...this.props} />
-            <Progress
-              className=""
-              innerClassName=""
-              value={(currentTime / duration) * 100 || 0}
-              {...this.props} />
+
+          <div className="player__controls">
+            <div className="player__controls-buttons">
+                <h2 className="">{tracks ? tracks.title : ''}</h2>
+                <PrevButton
+                className="player__button"
+                onPrevClick={this.prevIndex.bind(this)}
+                {...this.props} />
+                <PlayButton
+                className="player__button player__playbutton"
+                {...this.props} />
+                <NextButton
+                className="player__button"
+                onNextClick={this.nextIndex.bind(this)}
+                {...this.props} />
+                <VolumeControl
+                className="player__volume"
+                buttonClassName="player__button player__button--volume"
+                {...this.props} />
+            </div>
+            <div className="player__progress-container">
+                <Timer className="player__progress-timer" duration={duration || 0} currentTime={currentTime} {...this.props} />
+                <Progress
+                className="player__progress"
+                innerClassName="player__progress-inner"
+                value={(currentTime / duration) * 100 || 0}
+                {...this.props} />
+            </div>
           </div>
         </div>
-        {this.renderTrackList()}
+        <div className="player__track-list">
+            {this.renderTrackList()}
+        </div>
       </div>
     );
   }
