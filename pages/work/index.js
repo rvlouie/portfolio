894098e3e690e7bef02d2e018dc1e462828a34ea
Rick Louie <rvlouie@Ricks-Air.fios-router.home>
2019-07-08 12:11:10 -0400

    Minor auth

--- pages/work/index.js
+++ pages/work/index.js
@@ -9,21 +9,66 @@ class WorkPage extends Component {
     return { isServer };
   }
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      password: '',
+      validated: false
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange = event => {
+    this.setState({
+      password: event.target.value
+    });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    if (this.state.password == 'ricklouie') {
+      this.setState({
+        validated: true
+      })
+    }
+  }
+
   render() {
+    const validated = this.state.validated;
     return (
-      <main>
-        <Header />
-        <section>
-          <h3>
-            Work
-          </h3>
-        </section>
-        <section className="work__item">
-          <Link href="/work/rubber-tracks">
-            <a>Converse Rubber Tracks Sample Library</a>
-          </Link>
-        </section>
-      </main>
+
+    
+        <main>
+          <Header />
+          <div>
+            {!validated ? 
+              ( 
+                <form onSubmit={this.handleSubmit}>
+                  <input type="password" value={this.state.password} onChange={this.handleChange} /> 
+                  <button type="submit" value="Submit">Let me in!</button>
+                </form> 
+              ) :
+              (
+                <div>
+                  <section>
+                    <h3>
+                      Work
+                    </h3>
+                  </section>
+                  <section className="work__item">
+                    <Link href="/work/rubber-tracks">
+                      <a>Converse Rubber Tracks Sample Library</a>
+                    </Link>
+                  </section>
+                </div>
+              )
+            }
+          </div>
+        </main>
+
     );
   }
 }
