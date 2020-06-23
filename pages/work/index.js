f59d80aa40305ebdec49e8e88bc5e988c51e4658
rvlouie <rvlouie@gmail.com>
2020-06-23 18:10:37 -0400

    Fix images

--- pages/work/index.js
+++ pages/work/index.js
@@ -104,7 +104,7 @@ class WorkPage extends Component {
                 <label className="work__password-label">Need access? Reach out to rvlouie@gmail.com or @rvlouie on social media. Otherwise, enter the password!</label>
                 <input className="work__password-input" type="password" value={this.state.password} onChange={this.handleChange} /> 
                 {showErrorMessage && <div className="work__password-error">{errorMessage}</div>}
-                <button disabled={(password.length > 0) ? false : true} className={(password.length > 0) ? 'work__password-button' : 'work__password-button work__password-button--disabled'} type="submit" value="Submit">Show me the work</button>
+                <button disabled={(password.length > 0) ? false : true} className={(password.length > 0) ? 'work__password-button' : 'work__password-button work__password-button--disabled'} type="submit" value="Submit">Show me the work</button>      
               </form> 
             ) :
             (
